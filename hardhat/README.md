098197c99c0342430c48df3a21e596f8734a6093
obinnafranklinduru <obinnafranklinduru.com>
2022-12-03 12:30:03 +0100

    initial commit

--- hardhat/README.md
+++ hardhat/README.md
@@ -12,4 +12,11 @@ npx hardhat node
 npx hardhat run scripts/deploy.js
 ```
 
-- The FakeNFTMarketplace exposes some basic functions that we will be using from the DAO contract to purchase NFTs if a proposal is passed. A real NFT marketplace would be more complicated - as not all NFTs have the same price.
+ The FakeNFTMarketplace exposes some basic functions that we will be using from the DAO contract to purchase NFTs if a proposal is passed. A real NFT marketplace would be more complicated - as not all NFTs have the same price.
+
+### The functionality we need in the DAO contract
+
+- Store created proposals in the contract state
+- Allow holders of the BinnaDev NFT to create new proposals.
+- Allow holders of the BinnaDev NFT to vote on proposals, given that they haven't already voted and that the proposal hasn't passed its deadline yet.
+- Allow BinnaDev NFT holders to execute a proposal after its deadline has passed, triggering an NFT purchase if it does not.
--- hardhat/contracts/BinnaDevsDAO.sol
+++ hardhat/contracts/BinnaDevsDAO.sol
@@ -6,17 +6,87 @@ import "../interfaces/IBinnaDevsNFT.sol";
 import "../interfaces/IFakeNFTMarketplace.sol";
 
 contract BinnaDevsDAO is Ownable {
+    struct Proprosal {
+        // nftTokenId - the tokenID of the NFT to purchase from FakeNFTMarketplace
+        // if the proposal passes
+        uint256 nftTokenId;
+        // deadline -  Proposal can be executed after the deadline has been exceeded.
+        uint256 deadline;
+        // yayVotes - number of yes votes for this proposal   
+        uint256 yayVotes;
+        // nayVotes - number of no votes for this proposal
+        uint256 nayVotes;
+        // executed - whether or not this proposal has been executed yet.
+        // Cannot be executed before the deadline has been exceeded.
+        bool executed;
+        // voters - a mapping of BinnaDevsNFT tokenIDs to booleans indicating whether 
+        // that NFT has already been used to cast a vote or not
+        mapping(uint256 => bool) voter;
+    }
+
+    // Create a mapping of ID to Proposal to track all of the proposals
+    mapping(uint256 => Proprosal) public proprosals;
+
+    // Number of proposals that have been created
+    uint256 public numProposals;
+
     IFakeNFTMarketplace nftMarketplace;
     IBinnaDevsNFT binnaDevsNFT;
 
     /**
      * Create a payable constructor which initializes the contract
-     * instances for FakeNFTMarketplace and CryptoDevsNFT
+     * instances for FakeNFTMarketplace and BinnaDevsNFT
      * The payable allows this constructor to accept an ETH deposit when it is being deployed
      */
     constructor(address _nftMarketplace, address _binnaDevsNFT) payable {
         nftMarketplace = IFakeNFTMarketplace(_nftMarketplace);
         binnaDevsNFT = IBinnaDevsNFT(_binnaDevsNFT);
+    }
+
+    modifier nftHolderOnly(){
+        require(binnaDevNFT.balanceOf(msg.sender) > 0, "NOT_A_DAO_MEMBER");
+        _;
+    }
+
+    /**
+     * @dev createProposal allows a BinnaDevsNFT holder to create a new proposal in the DAO
+     * @param _nftTokenId - the tokenID of the NFT to be purchased from FakeNFTMarketplace 
+     * if this proposal passes
+     * 
+     * @return Returns the proposal index for the newly created proposal
+     */
+    function createProposal()
+    external
+    nftHolderOnly
+    {
+
+    }
+
+    /**
+     * @dev voteOnProposal allows a BinnaDevsNFT holder to cast 
+     * their vote on an active proposal.
+     * 
+     * @param proposalIndex - the index of the proposal to vote on in the proposals array
+     * @param vote - the type of vote they want to cast
+     */
+    function voteOnProposal()
+    external
+    nftHolderOnly
+    {
+
+    }
+
+    /**
+     * @dev executeProposal allows any BinnaDevsNFT holder to execute a proposal
+     * after it's deadline has been exceeded.
+     * 
+     * @param proposalIndex - the index of the proposal to execute in the proposals array
+     */
+    function executeProposal()
+    external
+    nftHolderOnly
+    {
+
     }
 
      // Function to receive Ether. msg.data must be empty
